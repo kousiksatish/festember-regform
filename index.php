74902b6fb6a1fc1c3a5c6ab6dda7d855815821a1
kousiksatish <kousiksatish@gmail.com>
2015-08-01 00:39:42 +0530

    Form added

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-
-<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -9,26 +7,25 @@
     <meta name="author" content="Delta Force">
     <link rel="icon" href="../../favicon.ico">
 
-    <title>Starter Template for Bootstrap</title>
+    <title>Festember Registration</title>
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-
     <style>
-      body {
+    body {
         padding-top: 50px;
       }
-      .starter-template {
+        .starter-template {
         padding: 40px 15px;
         //text-align: center;
       }
 
     </style>
+    
   </head>
 
   <body>
-
-    <nav class="navbar navbar-inverse navbar-fixed-top">
+  <nav class="navbar navbar-inverse navbar-fixed-top">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -47,23 +44,104 @@
       </div>
     </nav>
 
-    <div class="container">
-
-      <div class="starter-template">
-        <h1>ID Reg form</h1>
-        <p class="lead">
-          
-
-        </p>
+  <div class="container">
+  <div class="starter-template">
+    <form class="form-horizontal" enctype="multipart/form-data">
+    <div class="form-group">
+      <label for="fullname" class="col-sm-2 control-label">Full Name</label>
+      <div class="col-sm-3">
+        <input type="text" class="form-control" name="fullname" placeholder="Full Name" required>
       </div>
+    </div>
+    <div class="form-group">
+      <label for="idname" class="col-sm-2 control-label">ID Name</label>
+      <div class="col-sm-3">
+        <input type="text" class="form-control" name="idname" placeholder="ID Name" required>
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="teamname" class="col-sm-2 control-label">Team Name</label>
+      <div class="col-sm-3">
+        <select class="form-control" name="teamname" required>
+          <option value="marketing">Marketing</option>
+          <option value="publicity">Publicity</option>
+          <option value="workshops">Workshops</option>
+          <option value="events">Events</option>
+          <option value="csg">CSG</option>
+          <option value="fsr">FSR</option>
+        </select>
+      </div>
+    </div>
 
-    </div><!-- /.container -->
+    <div class="form-group">
+      <label for="year" class="col-sm-2 control-label">Year</label>
+      <div class="col-sm-3">
+        <select class="form-control" name="year" required>
+          <option value="2">II</option>
+          <option value="3">III</option>
+          <option value="4">IV</option>
+        </select>
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="designation" class="col-sm-2 control-label">Designation</label>
+      <div class="col-sm-3">
+        <select class="form-control" name="designation" required>
+          <option value="manager">Manager</option>
+          <option value="deputymanager">Deputy Manager</option>
+          <option value="coordinator">Coordinator</option>
+        </select>
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="rollno" class="col-sm-2 control-label">Roll Number</label>
+      <div class="col-sm-3">
+        <input type="text" class="form-control" name="rollno" placeholder="Roll Number" required>
+      </div>
+    </div>
 
+    <div class="form-group">
+      <label for="dept" class="col-sm-2 control-label">Department</label>
+      <div class="col-sm-3">
+        <select class="form-control" name="year" required>
+            <option value="arch">ARCH</option>
+            <option value="chem">CHEM</option>
+            <option value="civ">CIV</option>
+            <option value="cse">CSE</option>
+            <option value="ece">ECE</option>
+            <option value="eee">EEE</option>
+            <option value="ice">ICE</option>
+            <option value="mech">MECH</option>
+            <option value="meta">META</option>
+            <option value="prod">PROD</option>
+        </select>
+      </div>
+    </div>
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+    <div class="form-group">
+      <label for="email" class="col-sm-2 control-label">Email</label>
+      <div class="col-sm-3">
+        <input type="email" class="form-control" name="email" placeholder="Email" required>
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label for="photo" class="col-sm-2 control-label">Photo for ID</label>
+      <div class="col-sm-3">
+        <input type="file" class="form-control" name="photo" placeholder="File" required>
+      </div>
+    </div>
+
+    
+    <div class="form-group">
+      <div class="col-sm-offset-2 col-sm-3">
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </div>
+    </div>
+
+</form>
+</div>
+</div>
   </body>
+
 </html>
